1fd97e28b1fcb936b6a5f4512a993c73884d2584
ahmedmahalin <mahalinahmed@gmail.com>
2022-09-29 11:22:12 +0200

    überarbeitet

--- src/Comparable/Zusammenfassung.java
+++ src/Comparable/Zusammenfassung.java
@@ -3,10 +3,16 @@ package Comparable;
 import java.util.*;
 
 class Personen{
-    private String vorName; 
+    private String vorName;
+
     
-    public Personen(String varName){
+    public Personen(String vorName){
         this.vorName = vorName;
+    }
+
+    @Override
+    public String toString() {
+        return "Personen{" + "vorName='" + vorName + '\'' + '}';
     }
 }
 
@@ -15,7 +21,7 @@ class Personen{
 
 
 public class Zusammenfassung {
-    private static Object Collection;
+
 
     public static void main(String[] args) {
 
@@ -23,20 +29,21 @@ public class Zusammenfassung {
         List<Personen> list = new ArrayList<>();
 
         //
-        Zusammenfassung.addElements(3, list);
-        Zusammenfassung.getElements(list);
+       Zusammenfassung.addElements(4,list);
+       Zusammenfassung.getElements(list);
+
         //for(Ty ,parameternameist egal: Struktur)
 
 
-      Set<String> set = new TreeSet<>();
+
         System.out.println("___________");
-      // <lets get it>
+        Set<String> set = new TreeSet<>();
     }
                 //Collection ist entweder ein set oder list
         public static void addElements(int anzahlPerson, Collection<Personen> col){
         for(int i = 1; i <= anzahlPerson; i++){
             Scanner scanName = new Scanner(System.in);
-            System.out.println("Name eingeben");
+            System.out.println("Name eingeben: ");
             String eingabeName = scanName.next();
             Personen personen = new Personen(eingabeName); //Referenzvariable wo die varibale gespeichert wird
             col.add(personen);
@@ -47,9 +54,9 @@ public class Zusammenfassung {
 
         }
             public static void getElements(Collection<Personen> col){
-                for (Personen i : col){
-                    System.out.println("Name der Person");
-                    System.out.println(i);
+                for (Personen personen : col){
+                    System.out.print("Name der Person");
+                    System.out.println(personen);
                 }
             }
         }
